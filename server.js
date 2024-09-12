489528cdad0cb36fc1df4196402cd9011a07fa67
Mahmoud Zeus <meropvo17@gmail.com>
2024-09-12 14:39:20 +0300

    enable cors

--- server.js
+++ server.js
@@ -27,6 +27,19 @@ console.log(getUserPassword("mahmoud"));
 connectDB();
 const app = express();
 app.use(cors());
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "https://grddit-7f7df.web.app");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+});
 app.use(express.json());
 // use routes
 app.use("/api/recent", recentRoute);
