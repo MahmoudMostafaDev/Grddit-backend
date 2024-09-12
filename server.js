1859943abe682b568db11625fe82d908a47d8f24
Mahmoud Zeus <meropvo17@gmail.com>
2024-09-12 14:43:32 +0300

    setting the frontend in the cors of socket

--- server.js
+++ server.js
@@ -55,7 +55,7 @@ server.listen(5050);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://grddit-7f7df.web.app/",
     methods: ["GET", "POST"],
   },
 });
